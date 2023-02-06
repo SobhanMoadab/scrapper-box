40c67a6d172af7524153494ba0edea1048ab9909
Sobhan m <veiledassassin99@gmail.com>
2023-02-06 16:52:33 +0330

    [Test]: transferComment,should throw with invalid data

--- src/test/transfer-comments.spec.ts
+++ src/test/transfer-comments.spec.ts
@@ -1,12 +1,14 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../app.module';
 import { TransferCommentsUseCase } from '../usecases/transferComments/TransferComments';
+import { TransferCommentsDTO } from '../usecases/transferComments/TransferCommentsDTO';
 
 describe('Transfer comments useCase', () => {
   let useCase: TransferCommentsUseCase;
 
   beforeEach(async () => {
-    jest.setTimeout(10000);
+    jest.setTimeout(6000);
     const module: TestingModule = await Test.createTestingModule({
       providers: [],
       imports: [AppModule],
@@ -17,5 +19,17 @@ describe('Transfer comments useCase', () => {
 
   it('useCase should be defined', () => {
     expect(useCase).toBeDefined();
+  });
+
+  // given i provide invalid input, i should get error
+  it('should throw badrequest exception, when provided with invalid data', async () => {
+    const dto: TransferCommentsDTO = {
+      token: '',
+    };
+    // when i attempt to transfer comments
+    const result = await useCase.transferComments(dto);
+    // then i expect result to be instance of bad excetion class
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(BadRequestException);
   });
 });
--- src/usecases/transferComments/TransferComments.ts
+++ src/usecases/transferComments/TransferComments.ts
@@ -1,1 +1,22 @@
-export class TransferCommentsUseCase {}
+import { BadRequestException } from '@nestjs/common';
+import { Either, left, Result, right } from '../../core/Result';
+import { UnexpectedError } from '../../core/UnexpectedError';
+import { TransferCommentsDTO } from './TransferCommentsDTO';
+
+type Response = Either<
+  BadRequestException | UnexpectedError | Result<any>,
+  Result<void>
+>;
+export class TransferCommentsUseCase {
+  async transferComments(dto: TransferCommentsDTO): Promise<Response> {
+    try {
+      if (!dto.token) {
+        return left(new BadRequestException());
+      }
+
+      return right(Result.ok());
+    } catch (err) {
+      return left(new UnexpectedError('Something went wrong'));
+    }
+  }
+}
